08604b2e0feb95e2336a756ce48ae626cfea975c
John Kenneth Romero <jeromero@csmbataan.ph.education>
2020-09-23 23:24:21 +0400

    to see number of days added label

--- Date Calculator/MainWindow.xaml.cs
+++ Date Calculator/MainWindow.xaml.cs
@@ -23,6 +23,26 @@ namespace Date_Calculator
         public MainWindow()
         {
             InitializeComponent();
+      
+           
+        }
+
+        private void ___btnCalculate__Click(object sender, RoutedEventArgs e)
+        {
+            DateTime birthday = DateTime.Parse(dtBirthday.Text);
+            DateTime now = DateTime.Now;
+            string bday = birthday.Month + "/" + birthday.Day + "/2021";
+
+            DateTime nextBirthday2021 = DateTime.Parse(bday);
+            MessageBox.Show(bday);
+
+            double days = (nextBirthday2021 - now).TotalDays;
+            MessageBox.Show(days.ToString());
+
+            var Days = (nextBirthday2021 - now).TotalDays;
+            lbnDays.Content = "You are " + days + " days old";  
+      
+
         }
     }
 }
